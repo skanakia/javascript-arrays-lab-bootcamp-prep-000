23261cd579b553ad21a8b69bcfab908edc9891d2
Sameer Kanakia <skanakia1@gmail.com>
2018-03-21 19:57:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 const app = "I don't do much."
 var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKittens(name) {
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift(0)
+}
